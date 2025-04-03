18973fc4a50cd3e46efc684321e639642d7e0398
James Mackay <jmackay13@gmail.com>
2025-04-03 22:22:57 +0100

    Handling the splitting and extracting of markdown

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from textnode import TextNode 
 from textnode import TextType
 from leafnode import LeafNode
+import re
 
 def main():
     text_node = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
-    print(text_node)
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
@@ -39,5 +39,62 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        # Use regex to split text into parts (images and surrounding text)
+        pattern = r"!\[(.*?)\]\((.*?)\)"
+        last_end = 0
+        for match in re.finditer(pattern, old_node.text):
+            start, end = match.span()
+            # Add the text before the link as a TextNode
+            if start > last_end:
+                new_nodes.append(TextNode(old_node.text[last_end:start], TextType.TEXT))
+            # Add the link as a TextNode with TextType.LINK
+            link_text, url = match.groups()
+            new_nodes.append(TextNode(link_text, TextType.IMAGE, url))
+            last_end = end
+        # Add the remaining text after the last link
+        if last_end < len(old_node.text):
+            new_nodes.append(TextNode(old_node.text[last_end:], TextType.TEXT))
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        # Use regex to find links and their surrounding text
+        pattern = r"(?<!\!)\[(.*?)\]\((.*?)\)"
+        last_end = 0
+        for match in re.finditer(pattern, old_node.text):
+            start, end = match.span()
+            # Add the text before the link as a TextNode
+            if start > last_end:
+                new_nodes.append(TextNode(old_node.text[last_end:start], TextType.TEXT))
+            # Add the link as a TextNode with TextType.LINK
+            link_text, url = match.groups()
+            new_nodes.append(TextNode(link_text, TextType.LINK, url))
+            last_end = end
+        # Add the remaining text after the last link
+        if last_end < len(old_node.text):
+            new_nodes.append(TextNode(old_node.text[last_end:], TextType.TEXT))
+    return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def text_to_textnodes(text):
+    return split_nodes_delimiter(split_nodes_delimiter(split_nodes_delimiter(split_nodes_image(split_nodes_link(text)), "`", TextType.CODE), "_", TextType.ITALIC), "**", TextType.BOLD)
 
 main()
--- src/test_main.py
+++ src/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import split_nodes_delimiter
+from main import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextNode, TextType
 
 
@@ -82,6 +82,375 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+
+class TestMarkdownExtraction(unittest.TestCase):
+    def test_extract_markdown_images_single(self):
+        text = "Here is an image: ![Alt text](https://example.com/image.png)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("Alt text", "https://example.com/image.png")])
+
+    def test_extract_markdown_images_multiple(self):
+        text = (
+            "Here is the first image: ![Image1](https://example.com/image1.png) "
+            "and here is the second: ![Image2](https://example.com/image2.png)"
+        )
+        result = extract_markdown_images(text)
+        self.assertEqual(
+            result,
+            [
+                ("Image1", "https://example.com/image1.png"),
+                ("Image2", "https://example.com/image2.png"),
+            ],
+        )
+
+    def test_extract_markdown_images_none(self):
+        text = "This text has no images."
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [])
+
+    def test_extract_markdown_links_single(self):
+        text = "Here is a link: [Example](https://example.com)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("Example", "https://example.com")])
+
+    def test_extract_markdown_links_multiple(self):
+        text = (
+            "Here is the first link: [Google](https://google.com) "
+            "and here is the second: [GitHub](https://github.com)"
+        )
+        result = extract_markdown_links(text)
+        self.assertEqual(
+            result,
+            [
+                ("Google", "https://google.com"),
+                ("GitHub", "https://github.com"),
+            ],
+        )
+
+    def test_extract_markdown_links_none(self):
+        text = "This text has no links."
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [])
+
+    def test_extract_markdown_images_and_links(self):
+        text = (
+            "Here is a link: [Example](https://example.com) "
+            "and an image: ![Alt text](https://example.com/image.png)"
+        )
+        image_result = extract_markdown_images(text)
+        link_result = extract_markdown_links(text)
+        self.assertEqual(image_result, [("Alt text", "https://example.com/image.png")])
+        self.assertEqual(link_result, [("Example", "https://example.com")])
+
+    def test_extract_multiple_images(self):
+        text = (
+            "Here is the first image: ![Image1](https://example.com/image1.png) "
+            "and here is the second image: ![Image2](https://example.com/image2.png) "
+            "and a third image: ![Image3](https://example.com/image3.png)"
+        )
+        
+        result = extract_markdown_images(text)
+        self.assertEqual(
+            result,
+            [
+                ("Image1", "https://example.com/image1.png"),
+                ("Image2", "https://example.com/image2.png"),
+                ("Image3", "https://example.com/image3.png"),
+            ],
+        )
+
+    def test_extract_multiple_links(self):
+        text = (
+            "Here is the first link: [Google](https://google.com) "
+            "and here is the second link: [GitHub](https://github.com) "
+            "and a third link: [StackOverflow](https://stackoverflow.com)"
+        )
+        result = extract_markdown_links(text)
+        self.assertEqual(
+            result,
+            [
+                ("Google", "https://google.com"),
+                ("GitHub", "https://github.com"),
+                ("StackOverflow", "https://stackoverflow.com"),
+            ],
+        )
+
+    def test_extract_mixed_images_and_links(self):
+        text = (
+            "Here is a link: [Example](https://example.com) "
+            "and an image: ![Alt text](https://example.com/image.png) "
+            "another link: [GitHub](https://github.com) "
+            "and another image: ![Image2](https://example.com/image2.png)"
+        )
+        image_result = extract_markdown_images(text)
+        link_result = extract_markdown_links(text)
+        self.assertEqual(
+            image_result,
+            [
+                ("Alt text", "https://example.com/image.png"),
+                ("Image2", "https://example.com/image2.png"),
+            ],
+        )
+        self.assertEqual(
+            link_result,
+            [
+                ("Example", "https://example.com"),
+                ("GitHub", "https://github.com"),
+            ],
+        )
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+
+class TestSplitNodes(unittest.TestCase):
+    def test_split_nodes_image_single(self):
+        old_nodes = [TextNode("Here is an image: ![Alt text](https://example.com/image.png)", TextType.TEXT)]
+        new_nodes = split_nodes_image(old_nodes)
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual(new_nodes[0].text, "Here is an image: ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "Alt text")
+        self.assertEqual(new_nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[1].url, "https://example.com/image.png")
+
+    def test_split_nodes_image_multiple(self):
+        old_nodes = [
+            TextNode(
+                "Here is the first image: ![Image1](https://example.com/image1.png) "
+                "and here is the second image: ![Image2](https://example.com/image2.png)",
+                TextType.TEXT,
+            )
+        ]
+        new_nodes = split_nodes_image(old_nodes)
+        self.assertEqual(len(new_nodes), 4)
+        self.assertEqual(new_nodes[0].text, "Here is the first image: ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "Image1")
+        self.assertEqual(new_nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[1].url, "https://example.com/image1.png")
+        self.assertEqual(new_nodes[2].text, " and here is the second image: ")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[3].text, "Image2")
+        self.assertEqual(new_nodes[3].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[3].url, "https://example.com/image2.png")
+
+    def test_split_nodes_link_single(self):
+        old_nodes = [TextNode("Here is a link: [Example](https://example.com)", TextType.TEXT)]
+        new_nodes = split_nodes_link(old_nodes)
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual(new_nodes[0].text, "Here is a link: ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "Example")
+        self.assertEqual(new_nodes[1].text_type, TextType.LINK)
+        self.assertEqual(new_nodes[1].url, "https://example.com")
+
+    def test_split_nodes_link_multiple(self):
+        old_nodes = [
+            TextNode(
+                "Here is the first link: [Google](https://google.com) "
+                "and here is the second link: [GitHub](https://github.com)",
+                TextType.TEXT,
+            )
+        ]
+        new_nodes = split_nodes_link(old_nodes)
+        self.assertEqual(len(new_nodes), 4)
+        self.assertEqual(new_nodes[0].text, "Here is the first link: ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "Google")
+        self.assertEqual(new_nodes[1].text_type, TextType.LINK)
+        self.assertEqual(new_nodes[1].url, "https://google.com")
+        self.assertEqual(new_nodes[2].text, " and here is the second link: ")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[3].text, "GitHub")
+        self.assertEqual(new_nodes[3].text_type, TextType.LINK)
+        self.assertEqual(new_nodes[3].url, "https://github.com")
+
+    def test_split_nodes_image_and_text(self):
+        old_nodes = [
+            TextNode(
+                "Text before image ![Alt text](https://example.com/image.png) text after image.",
+                TextType.TEXT,
+            )
+        ]
+        new_nodes = split_nodes_image(old_nodes)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "Text before image ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "Alt text")
+        self.assertEqual(new_nodes[1].text_type, TextType.IMAGE)
+        self.assertEqual(new_nodes[1].url, "https://example.com/image.png")
+        self.assertEqual(new_nodes[2].text, " text after image.")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_link_and_text(self):
+        old_nodes = [
+            TextNode(
+                "Text before link [Example](https://example.com) text after link.",
+                TextType.TEXT,
+            )
+        ]
+        new_nodes = split_nodes_link(old_nodes)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "Text before link ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "Example")
+        self.assertEqual(new_nodes[1].text_type, TextType.LINK)
+        self.assertEqual(new_nodes[1].url, "https://example.com")
+        self.assertEqual(new_nodes[2].text, " text after link.")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_plain_text(self):
+        text = "This is plain text."
+        result = text_to_textnodes([TextNode(text, TextType.TEXT)])
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].text, "This is plain text.")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+
+    def test_bold_text(self):
+        text = "This is **bold** text."
+        result = text_to_textnodes([TextNode(text, TextType.TEXT)])
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "bold")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, " text.")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_italic_text(self):
+        text = "This is _italic_ text."
+        result = text_to_textnodes([TextNode(text, TextType.TEXT)])
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "italic")
+        self.assertEqual(result[1].text_type, TextType.ITALIC)
+        self.assertEqual(result[2].text, " text.")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_code_text(self):
+        text = "This is `code` text."
+        result = text_to_textnodes([TextNode(text, TextType.TEXT)])
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "code")
+        self.assertEqual(result[1].text_type, TextType.CODE)
+        self.assertEqual(result[2].text, " text.")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_links(self):
+        text = "This is a [link](https://example.com)."
+        result = text_to_textnodes([TextNode(text, TextType.TEXT)])
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This is a ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "link")
+        self.assertEqual(result[1].text_type, TextType.LINK)
+        self.assertEqual(result[1].url, "https://example.com")
+        self.assertEqual(result[2].text, ".")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_images(self):
+        text = "This is an image: ![Alt text](https://example.com/image.png)."
+        result = text_to_textnodes([TextNode(text, TextType.TEXT)])
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "This is an image: ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "Alt text")
+        self.assertEqual(result[1].text_type, TextType.IMAGE)
+        self.assertEqual(result[1].url, "https://example.com/image.png")
+        self.assertEqual(result[2].text, ".")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_combined_formatting(self):
+        text = "This is **bold**, _italic_, `code`, [link](https://example.com), and ![image](https://example.com/image.png)."
+        result = text_to_textnodes([TextNode(text, TextType.TEXT)])
+        self.assertEqual(len(result), 11)
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "bold")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+        self.assertEqual(result[2].text, ", ")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+        self.assertEqual(result[3].text, "italic")
+        self.assertEqual(result[3].text_type, TextType.ITALIC)
+        self.assertEqual(result[4].text, ", ")
+        self.assertEqual(result[4].text_type, TextType.TEXT)
+        self.assertEqual(result[5].text, "code")
+        self.assertEqual(result[5].text_type, TextType.CODE)
+        self.assertEqual(result[6].text, ", ")
+        self.assertEqual(result[6].text_type, TextType.TEXT)
+        self.assertEqual(result[7].text, "link")
+        self.assertEqual(result[7].text_type, TextType.LINK)
+        self.assertEqual(result[7].url, "https://example.com")
+        self.assertEqual(result[8].text, ", and ")
+        self.assertEqual(result[8].text_type, TextType.TEXT)
+        self.assertEqual(result[9].text, "image")
+        self.assertEqual(result[9].text_type, TextType.IMAGE)
+        self.assertEqual(result[9].url, "https://example.com/image.png")
+        self.assertEqual(result[10].text, ".")
+        self.assertEqual(result[10].text_type, TextType.TEXT)
+
+    def test_complex_text_with_all_formats(self):
+        text = (
+            "This is **text** with an _italic_ word and a `code block` and an "
+            "![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        )
+        result = text_to_textnodes([TextNode(text, TextType.TEXT)])
+        self.assertEqual(len(result), 10)
+
+        # Assertions for each part of the text
+        self.assertEqual(result[0].text, "This is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+
+        self.assertEqual(result[1].text, "text")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+
+        self.assertEqual(result[2].text, " with an ")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+        self.assertEqual(result[3].text, "italic")
+        self.assertEqual(result[3].text_type, TextType.ITALIC)
+
+        self.assertEqual(result[4].text, " word and a ")
+        self.assertEqual(result[4].text_type, TextType.TEXT)
+
+        self.assertEqual(result[5].text, "code block")
+        self.assertEqual(result[5].text_type, TextType.CODE)
+
+        self.assertEqual(result[6].text, " and an ")
+        self.assertEqual(result[6].text_type, TextType.TEXT)
+
+        self.assertEqual(result[7].text, "obi wan image")
+        self.assertEqual(result[7].text_type, TextType.IMAGE)
+        self.assertEqual(result[7].url, "https://i.imgur.com/fJRm4Vk.jpeg")
+
+        self.assertEqual(result[8].text, " and a ")
+        self.assertEqual(result[8].text_type, TextType.TEXT)
+
+        self.assertEqual(result[9].text, "link")
+        self.assertEqual(result[9].text_type, TextType.LINK)
+        self.assertEqual(result[9].url, "https://boot.dev")
 
 
 if __name__ == "__main__":
